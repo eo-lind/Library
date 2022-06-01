baaa8c7917ae50ae00ca11a2e789723edeca0a02
Olivia Lind <eo.lind@gmail.com>
2022-05-31 21:39:21 -0500

    step 7 completed

--- Program.cs
+++ Program.cs
@@ -7,8 +7,10 @@ namespace Library
         static void Main(string[] args)
         {
             Book newBook1 = new Book("The Amazing Adventures of Kavalier and Clay", "Michael Chabon", "Fiction");
+            Book newBook2 = new Book("Rosemary's Baby", "Ira Levin", "Fiction");
+            Book newBook3 = new Book("Valley of the Dolls", "Jacqueline Susann", "Fiction");
 
-                Console.WriteLine($"{newBook1.Title} by {newBook1.Author} ({newBook1.Genre}");
+                Console.WriteLine($"{newBook1.Title}, by {newBook1.Author}");
         }
     }
 }
